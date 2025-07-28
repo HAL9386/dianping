a7714f039080763e2092699fae990ac8eda41e2f
hal <mad024039@gmail.com>
2025-07-28 16:47:42 +0800

    feat(shop): 更新商铺信息时删除redis缓存维护一致性
    先操作数据库再操作redis。利用事务同步回调在事务提交后
    删除缓存。
    1. 先操作缓存后数据库。
      数据库中暂时为旧数据。更新线程先删除缓存，此时查询
      线程查询到旧数据并存入缓存，更新线程更新数据库为新
      数据。此时，数据不一致。
    2. 先操作数据库后缓存。
      缓存中暂时为旧数据。只有当缓存失效清空时，查询线程
      缓存未命中，查数据库得到旧数据；更新线程更新数据库
      再删除缓存；查询线程再将旧数据写入缓存。此时，数据
      不一致。

--- src/main/java/com/dp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/dp/service/impl/ShopServiceImpl.java
@@ -11,6 +11,9 @@ import com.dp.mapper.ShopMapper;
 import com.dp.service.IShopService;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.util.concurrent.TimeUnit;
 
@@ -45,5 +48,32 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     // 存在，写入缓存
     redisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), RedisConstant.CACHE_SHOP_TTL, TimeUnit.MINUTES);
     return Result.ok(shop);
+  }
+
+  /**
+   * 更新商铺信息
+   *
+   * @param shop 商铺数据
+   * @return 无
+   */
+  @Transactional(rollbackFor = Exception.class)
+  @Override
+  public Result update(Shop shop) {
+    if (shop.getId() == null) {
+      return Result.fail(MessageConstant.SHOP_NOT_EXIST);
+    }
+    // 更新数据库
+    updateById(shop);
+    // 删除缓存
+    // 如果删除缓存后发生异常，数据库回滚但redis未删除，会导致缓存与数据库不一致
+    TransactionSynchronizationManager.registerSynchronization(
+      new TransactionSynchronization() {
+        @Override
+        public void afterCommit() {
+          redisTemplate.delete(RedisConstant.CACHE_SHOP_KEY + shop.getId());
+        }
+      }
+    );
+    return Result.ok();
   }
 }
