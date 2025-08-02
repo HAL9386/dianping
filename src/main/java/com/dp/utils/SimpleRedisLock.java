7141f3b6bf2750985c73bf59d9a721a5207c8115
hal <mad024039@gmail.com>
2025-08-02 19:22:44 +0800

    feat(util): 进一步解决分布式锁的误删问题
    在释放锁时，判断锁标识和删除锁并不是原子的。
    如果线程A在判断锁标识是自己的后，阻塞了（fullgc）。
    锁过期删除，线程B获取到锁，此时线程A继续删除锁，互斥性被破坏。
    通过lua脚本编写判断持有者逻辑和删除逻辑，确保释放锁的原子性。

--- src/main/java/com/dp/utils/SimpleRedisLock.java
+++ src/main/java/com/dp/utils/SimpleRedisLock.java
@@ -1,9 +1,13 @@
 package com.dp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
 
 public class SimpleRedisLock implements ILock {
   private final String name;
@@ -18,7 +22,14 @@ public class SimpleRedisLock implements ILock {
 //  并保存到线程。释放时还要释放该资源。
 //  这样，一个服务器内部调用工具类获取到的都是同一个uuid，再拼上线程id，
 //  做到唯一。
-  public static final String THREAD_ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+  private static final String THREAD_ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+  private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+  static {
+    UNLOCK_SCRIPT = new DefaultRedisScript<>();
+    UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+    UNLOCK_SCRIPT.setResultType(Long.class);
+  }
 
   public SimpleRedisLock(String name, StringRedisTemplate redisTemplate) {
     this.name = name;
@@ -34,10 +45,16 @@ public class SimpleRedisLock implements ILock {
 
   @Override
   public void unlock() {
-    String threadId = THREAD_ID_PREFIX + Thread.currentThread().getId();
-    String lockKey = LOCK_KEY_PREFIX + this.name;
-    if (threadId.equals(redisTemplate.opsForValue().get(lockKey))) {
-      redisTemplate.delete(lockKey);
-    }
+//    String threadId = THREAD_ID_PREFIX + Thread.currentThread().getId();
+//    String lockKey = LOCK_KEY_PREFIX + this.name;
+//    if (threadId.equals(redisTemplate.opsForValue().get(lockKey))) {
+//      redisTemplate.delete(lockKey);
+//    }
+
+    redisTemplate.execute(
+      UNLOCK_SCRIPT,
+      List.of(LOCK_KEY_PREFIX + name),
+      THREAD_ID_PREFIX + Thread.currentThread().getId()
+    );
   }
 }
