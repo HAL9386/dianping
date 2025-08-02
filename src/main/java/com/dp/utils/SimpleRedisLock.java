8d6fffd15b113b190ca0400c7c51127ff2da5a08
hal <mad024039@gmail.com>
2025-08-02 11:23:39 +0800

    feat(util): 分布式锁加上线程的唯一标识
    如果A线程执行业务耗时久，锁过期释放。
    B线程就可以获取锁执行业务，随后A线程又释放锁。
    其他线程就可以抢锁执行，并没有保证操作的原子性以及操作的互斥性。
    加锁时存入一个唯一值，使用uuid，并在工具类内部维护。单一职责不影响
    外部业务的代码，否则还要在外部生成uuid再传入工具类加锁，
    并保存到线程。释放时还要释放该资源。
    这样，一个服务器内部调用工具类获取到的都是同一个uuid，再拼上线程id，
    做到唯一。

--- src/main/java/com/dp/utils/SimpleRedisLock.java
+++ src/main/java/com/dp/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.dp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -8,6 +9,8 @@ public class SimpleRedisLock implements ILock {
   private final String name;
   private final StringRedisTemplate redisTemplate;
   private static final String LOCK_KEY_PREFIX = "lock:";
+  // 一个服务器共用一个线程ID前缀
+  public static final String THREAD_ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
   public SimpleRedisLock(String name, StringRedisTemplate redisTemplate) {
     this.name = name;
@@ -16,13 +19,17 @@ public class SimpleRedisLock implements ILock {
 
   @Override
   public boolean tryLock(long timeoutSec) {
-    long threadId = Thread.currentThread().getId();
-    Boolean success = redisTemplate.opsForValue().setIfAbsent(LOCK_KEY_PREFIX + name, Long.toString(threadId), timeoutSec, TimeUnit.SECONDS);
+    String threadId = THREAD_ID_PREFIX + Thread.currentThread().getId();
+    Boolean success = redisTemplate.opsForValue().setIfAbsent(LOCK_KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
     return Boolean.TRUE.equals(success);
   }
 
   @Override
   public void unlock() {
-    redisTemplate.delete(LOCK_KEY_PREFIX + this.name);
+    String threadId = THREAD_ID_PREFIX + Thread.currentThread().getId();
+    String lockKey = LOCK_KEY_PREFIX + this.name;
+    if (threadId.equals(redisTemplate.opsForValue().get(lockKey))) {
+      redisTemplate.delete(lockKey);
+    }
   }
 }
